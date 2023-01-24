5ee4f37613f7256ea20c566bffa2a42ca82226a9
Frank <841290355@qq.com>
2023-01-24 21:55:39 +0800

    feat: 封装axios

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script setup lang="ts">
+import hyRequest from "@/service";
+hyRequest
+  .get({
+    url: "/home/multidata"
+  })
+  .then((res) => {
+    console.log(res);
+  });
 </script>
 
 <style lang="less" scoped>
