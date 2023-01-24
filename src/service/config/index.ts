a45768189909d0acd6171f089bef471f7613cac7
Frank <841290355@qq.com>
2023-01-24 22:34:27 +0800

    feat: 环境变量

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,12 @@
-export const BASE_URL = "http://codercba.com:8000";
+let BASE_URL = "";
+
+if (import.meta.env.DEV) {
+  BASE_URL = "http://codercba.com:8000";
+} else if (import.meta.env.PROD) {
+  BASE_URL = "http://codercba.com:8000";
+} else if (import.meta.env.SSR) {
+  BASE_URL = "http://codercba.com:8000";
+}
+
 export const TIME_OUT = 10000;
+export { BASE_URL };
