180a2fc850be9f2eded4ea881df49360cb40ff93
Frank <841290355@qq.com>
2023-01-24 20:39:50 +0800

    feat: pinia test打通

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,11 +9,11 @@ const router = createRouter({
     },
     {
       path: "/login",
-      component: () => import("@/views/login/login.vue")
+      component: () => import("@/views/login/Login.vue")
     },
     {
       path: "/main",
-      component: () => import("@/views/main/main.vue")
+      component: () => import("@/views/main/Main.vue")
     },
     {
       path: "/:pathMatch(.*)",
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,8 +1,16 @@
 <template>
   <div>main</div>
+  {{ counterStore.counter }}--{{ counterStore.doubleCounter }}
+  <button @click="changeCounter">修改counter</button>
 </template>
 
 <script setup lang="ts">
+import useCounterStore from "@/store/counter";
+const counterStore = useCounterStore();
+
+function changeCounter() {
+  counterStore.changeCounterAction(300);
+}
 </script>
 
 <style lang="less" scoped>
