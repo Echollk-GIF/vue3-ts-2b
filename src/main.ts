b69bc5f96f09d747dd055fac5e3d03d7ca50cd11
Frank <841290355@qq.com>
2023-01-24 23:05:40 +0800

    feat: Element-plus按需引入

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import "./assets/css/index.less";
 import App from "./App.vue";
 import router from "./router";
 import pinia from "./store";
+
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
